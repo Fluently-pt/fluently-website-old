06a39c958b6ac8f9ab292e15bd16a3a0608c0c83
kauredo <vaskafig@gmail.com>
2025-03-31 19:05:42 +0100

    add basic

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,44 @@
+import { useEffect } from "react";
 import { WaitlistForm } from "./WaitlistForm";
+import Swiper from "swiper";
 
 export function Hero() {
+  useEffect(() => {
+    const swiper = new Swiper(".hero-carousel", {
+      loop: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      effect: "fade",
+      speed: 1000,
+    });
+
+    return () => {
+      swiper.destroy();
+    };
+  }, []);
+
   return (
-    <div className="relative bg-gradient-to-b from-brand-50 to-white pt-44 pb-32 overflow-hidden">
+    <div className="relative pt-44 pb-32 overflow-hidden">
+      {/* Carousel */}
+      <div className="swiper hero-carousel absolute inset-0">
+        <div className="swiper-wrapper">
+          <div className="swiper-slide">
+            <picture>
+              <source
+                media="(max-width: 768px)"
+                srcSet="/img/hero-bg-2-mobile.webp"
+              />
+              <source media="(min-width: 769px)" srcSet="/img/hero-bg-2.webp" />
+              <img src="/img/hero-bg-2.webp" alt="" className="hero-bg" />
+            </picture>
+          </div>
+          {/* Add other slides similarly */}
+        </div>
+      </div>
+
+      {/* Existing content */}
       <div className="absolute inset-0">
         <div className="absolute -top-40 -right-40 w-96 h-96 rounded-full bg-brand-200 blur-3xl"></div>
         <div className="absolute -bottom-40 -left-40 w-96 h-96 rounded-full bg-brand-300/30 blur-3xl"></div>
@@ -26,6 +62,30 @@ export function Hero() {
           </div>
         </div>
       </div>
+
+      {/* Wave SVG */}
+      <svg
+        className="absolute bottom-0 w-full"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 24 150 28"
+        preserveAspectRatio="none"
+      >
+        <defs>
+          <path
+            id="wave-path"
+            d="M-160 44c30 0 58-18 88-18s58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+          ></path>
+        </defs>
+        <g className="wave1">
+          <use xlinkHref="#wave-path" x="50" y="3"></use>
+        </g>
+        <g className="wave2">
+          <use xlinkHref="#wave-path" x="50" y="0"></use>
+        </g>
+        <g className="wave3">
+          <use xlinkHref="#wave-path" x="50" y="9"></use>
+        </g>
+      </svg>
     </div>
   );
 }
