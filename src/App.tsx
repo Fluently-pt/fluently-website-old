031f8d22b91285ee9e053494c02e013cc539f434
kauredo <vaskafig@gmail.com>
2025-03-31 19:35:39 +0100

    start features

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ export default function App() {
       <Header />
       <main>
         <Hero />
-        <Stats />
         <Features />
+        <Stats />
         <WaitlistSection />
       </main>
       <Footer />
